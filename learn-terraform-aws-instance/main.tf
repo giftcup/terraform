4c18d2e501c5daaf2b67b55efce618c4f810f1be
giftcup <salometambe@gmail.com>
2023-08-16 16:17:47 +0100

    changing format

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "Tok237"
+    workspaces {
+      name = "learn-tf-aws"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
